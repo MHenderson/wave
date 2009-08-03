37bb1248ccb699fceda4c476ff0bf8d333562b5f
Matthew Henderson <matthew@ehrhart.(none)>
2009-08-03 19:50:55 -0400

    First prototype of WAVE graphical interface.

---
+++ Tool/gui/wave-gui.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+ 
+import wx
+import images
+
+MAIN_FRAME_SIZE = (600, 600)
+MAIN_FRAME_TITLE = "WAVe (Whole Architecture Verification)"
+
+class MainFrame(wx.Frame):
+
+    def __init__(self, parent, id):
+        wx.Frame.__init__(self, parent, id, title = MAIN_FRAME_TITLE, size = MAIN_FRAME_SIZE)
+        panel = wx.Panel(self)
+        panel.SetBackgroundColour('White')
+        statusBar = self.CreateStatusBar()
+        toolbar = self.CreateToolBar()
+        toolbar.AddSimpleTool(wx.NewId(), images.getNewBitmap(), "New", "Long help for 'New'")
+        toolbar.Realize()
+        menuBar = wx.MenuBar()
+        models_menu = wx.Menu()
+        models_menu.Append(wx.NewId(), "&Open", "Open")
+        models_menu.Append(wx.NewId(), "&Save", "Save")
+        models_menu.Append(wx.NewId(), "E&xit", "Exit")
+        menuBar.Append(models_menu, "&Models")
+        metamodels_menu = wx.Menu()
+        metamodels_menu.Append(wx.NewId(), "&Import", "Copy")
+        metamodels_menu.Append(wx.NewId(), "&Export", "Copy")
+        metamodels_menu.AppendSeparator()
+        metamodels_menu.Append(wx.NewId(), "&MM2 (Current metamodel)", "Display Options")
+        menuBar.Append(metamodels_menu, "M&eta-models")
+        scripts_menu = wx.Menu()
+        scripts_menu.Append(wx.NewId(), "d&r", "Dangling requires")
+        scripts_menu.Append(wx.NewId(), "d&s", "Dangling supplies")
+        scripts_menu.AppendSeparator()
+        scripts_menu.Append(wx.NewId(), "Export HTML report", "")
+        menuBar.Append(scripts_menu, "&Scripts")
+        self.SetMenuBar(menuBar)
+
+app = wx.PySimpleApp()
+frame = MainFrame(None, -1)
+frame.Show(1)
+app.MainLoop()
