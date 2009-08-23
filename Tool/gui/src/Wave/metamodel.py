e5268079f2cd1200fe7fb2c00a211f03071a3217
Matthew Henderson <matthew@ehrhart.(none)>
2009-08-23 19:58:19 -0400

    Fixing the import-metamodel handler.
      - An imported metamodel now appears as a submenu of the
        metamodels menu.

--- Tool/gui/src/Wave/metamodel.py
+++ Tool/gui/src/Wave/metamodel.py
@@ -3,12 +3,6 @@ import MM2
 import Wave
 from Wave import handlers
 
-def on_dr(self, event):
-    Wave.handlers.ternary_function_to_selected_pages(self, MM2.DR, 'dr')
-
-def on_ds(self, event):
-    Wave.handlers.binary_function_to_selected_pages(self, MM2.DS, 'ds')
-
 class WaveMetamodelMenu():
 
     def __init__(self, frame):
@@ -17,12 +11,13 @@ class WaveMetamodelMenu():
         self.bind_handlers()
 
     def init_menus(self):
-        self.frame.scripts_menu = wx.Menu()
-        self.frame.dr_menu_item = self.frame.scripts_menu.Append(wx.NewId(), "d&r", "Dangling requires")
-        self.frame.ds_menu_item = self.frame.scripts_menu.Append(wx.NewId(), "d&s", "Dangling supplies")
-        self.frame.menuBar.Append(self.frame.scripts_menu, "MM2")  
+        self.frame.mm2_sub_menu = wx.Menu()
+        self.frame.dr_menu_item = self.frame.mm2_sub_menu.Append(wx.NewId(), "d&r", "Dangling requires")
+        self.frame.ds_menu_item = self.frame.mm2_sub_menu.Append(wx.NewId(), "d&s", "Dangling supplies")
+        self.frame.metamodels_menu.AppendSeparator()
+        self.frame.metamodels_menu.AppendMenu(-1, "MM2", self.frame.mm2_sub_menu)  
 
     def bind_handlers(self):
-        self.frame.Bind(wx.EVT_MENU, on_dr, self.frame.dr_menu_item)
-        self.frame.Bind(wx.EVT_MENU, on_ds, self.frame.ds_menu_item)
+        self.frame.Bind(wx.EVT_MENU, self.frame.on_dr, self.frame.dr_menu_item)
+        self.frame.Bind(wx.EVT_MENU, self.frame.on_ds, self.frame.ds_menu_item)
 
