ff07f41f5afb3f81947d630e18d3576d3ea7a2d9
Matthew Henderson <matthew@ehrhart.(none)>
2009-08-23 22:13:21 -0400

    Attempting to generalize the creation of metamodel menus.

--- Tool/gui/src/Wave/metamodel.py
+++ Tool/gui/src/Wave/metamodel.py
@@ -3,27 +3,48 @@ import MM2
 import Wave
 from Wave import handlers
 
+class WaveMetamodelFunction():
+
+    def __init__(self, function, name, handler, long_name):
+        self.function = function
+        self.name = name
+        self.handler = handler
+        self.long_name = long_name
+
+class WaveMetamodel():
+
+    def __init__(self, name, functions):
+        self.name = name
+        self.functions = functions
+
+DR = WaveMetamodelFunction(MM2.DR, 'dr', Wave.handlers.ternary_function_to_selected_pages, 'dangling requires')
+DS = WaveMetamodelFunction(MM2.DS, 'ds', Wave.handlers.binary_function_to_selected_pages, 'dangling supplies')
+
+F1 = [DR, DS]
+mm2 = WaveMetamodel('MM2', F1)
+
 class WaveMetamodelMenu():
 
-    def __init__(self, frame):
+    def __init__(self, frame, metamodel = mm2):
         self.frame = frame
+        self.metamodel = metamodel
         self.init_menus()
         self.bind_handlers()
 
+    def create_handler(self, frame, wave_function):
+        def on(event):
+            wave_function.handler(frame, wave_function.function, wave_function.name)
+        return on
+
     def init_menus(self):
-        self.frame.mm2_sub_menu = wx.Menu()
-        self.frame.dr_menu_item = self.frame.mm2_sub_menu.Append(wx.NewId(), "d&r", "Dangling requires")
-        self.frame.ds_menu_item = self.frame.mm2_sub_menu.Append(wx.NewId(), "d&s", "Dangling supplies")
+        self.frame.mm_sub_menu = wx.Menu()
+        self.frame.mm_sub_menu_items = {}
+        for wave_function in self.metamodel.functions:
+            self.frame.mm_sub_menu_items[wave_function.name] = self.frame.mm_sub_menu.Append(wx.NewId(), wave_function.name, wave_function.long_name)
         self.frame.metamodels_menu.AppendSeparator()
-        self.frame.metamodels_menu.AppendMenu(-1, "MM2", self.frame.mm2_sub_menu)  
-            
-    def on_dr(self, event):
-        Wave.handlers.ternary_function_to_selected_pages(self.frame, MM2.DR, 'dr')
-        
-    def on_ds(self, event):
-        Wave.handlers.binary_function_to_selected_pages(self.frame, MM2.DS, 'ds')
+        self.frame.metamodels_menu.AppendMenu(-1, self.metamodel.name, self.frame.mm_sub_menu)  
 
     def bind_handlers(self):
-        self.frame.Bind(wx.EVT_MENU, self.on_dr, self.frame.dr_menu_item)
-        self.frame.Bind(wx.EVT_MENU, self.on_ds, self.frame.ds_menu_item)
+        for wave_function in self.metamodel.functions:
+            self.frame.Bind(wx.EVT_MENU, self.create_handler(self.frame, wave_function), self.frame.mm_sub_menu_items[wave_function.name])
 
