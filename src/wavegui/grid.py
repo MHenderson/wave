d403c1a8c8c172b215b08424a425a112d2ad0dc9
MHenderson <matthew.james.henderson@gmail.com>
2010-12-27 12:40:35 +0000

    Moved grid components to wavegui module.

--- src/wavegui/grid.py
+++ src/wavegui/grid.py
@@ -91,3 +91,145 @@ def apply_to_grid_tables(operation, *grid_tables):
     result_grid_table = WaveGridTable(result_relation)
     return result_grid_table
 
+
+class UnaryPrefixOperatorToCurrentPage:
+
+    def __init__(self, operator, prefix):
+        self.operator = operator
+        self.prefix = prefix
+
+    def __call__(self, frame):
+        current_page_index = frame.notebook.GetSelection()
+        result_name = self.prefix + frame.notebook.GetPageText(current_page_index)
+        current_grid_table = frame.current_grid_table()        
+        result_grid_table = Wave.grid.apply_to_grid_tables(self.operator, current_grid_table)
+        frame.notebook.new_page(result_grid_table, result_name)
+
+class BinaryInfixOperatorToSelectedPages: 
+
+    def __init__(self, operator, infix):
+        self.operator = operator
+        self.infix = infix
+
+    def __call__(self, frame):
+        page_index_1 = frame.select_page_index()
+        page_index_2 = frame.select_page_index()
+        result_name = frame.notebook.GetPageText(page_index_1) + self.infix + frame.notebook.GetPageText(page_index_2)
+        grid_table_1 = frame.get_grid_table(page_index_1)
+        grid_table_2 = frame.get_grid_table(page_index_2)
+        result_grid_table = Wave.grid.apply_to_grid_tables(self.operator, grid_table_1, grid_table_2)
+        frame.notebook.new_page(result_grid_table, result_name)
+
+class BinaryFunctionToSelectedPages:
+
+    def __init__(self, function, name):
+        self.function = function
+        self.name = name
+
+    def __call__(self, frame):
+        page_index_1 = frame.select_page_index()
+        page_index_2 = frame.select_page_index()
+        name_1 = frame.notebook.GetPageText(page_index_1)
+        name_2 = frame.notebook.GetPageText(page_index_2)
+        grid_table_1 = frame.get_grid_table(page_index_1)
+        grid_table_2 = frame.get_grid_table(page_index_2)
+        result_name = self.name + '(' + name_1 + ', ' + name_2 + ')'
+        result_grid_table = Wave.grid.apply_to_grid_tables(self.function, grid_table_1, grid_table_2)
+        frame.notebook.new_page(result_grid_table, result_name)
+
+class TernaryFunctionToSelectedPages:
+
+    def __init__(self, function, name):
+        self.function = function 
+        self.name = name
+
+    def __call__(self, frame):
+        page_index_1 = frame.select_page_index()
+        page_index_2 = frame.select_page_index()
+        page_index_3 = frame.select_page_index()
+        name_1 = frame.notebook.GetPageText(page_index_1)
+        name_2 = frame.notebook.GetPageText(page_index_2)
+        name_3 = frame.notebook.GetPageText(page_index_3)
+        grid_table_1 = frame.get_grid_table(page_index_1)
+        grid_table_2 = frame.get_grid_table(page_index_2)
+        grid_table_3 = frame.get_grid_table(page_index_3)
+        result_name = self.name + '(' + name_1 + ', ' + name_2 + ', ' + name_3 + ')'
+        result_grid_table = Wave.grid.apply_to_grid_tables(self.function, grid_table_1, grid_table_2, grid_table_3)
+        frame.notebook.new_page(result_grid_table, result_name)
+
+
+class FunctionWithCallStrategy():
+
+    def __init__(self, function):
+        self.function = function
+        self.handler = self.apply_strategy()
+        
+    def get_handler(self):
+        return self.handler
+
+    def apply_strategy(self):
+        nargs = self.function.nargs()
+        if nargs == 1:
+            return self.unary_strategy()
+        elif nargs == 2:
+            return self.binary_strategy()
+        elif nargs == 3:
+            return self.ternary_strategy()
+        else:
+            pass
+
+    def unary_strategy(self):
+        if ((self.function.fix == 'pre') and self.function.symbol):
+            return UnaryPrefixOperatorToCurrentPage(self.function, self.function.symbol)
+        else:
+            raise Wave.exceptions.WaveMissingCallStrategy
+
+    def binary_strategy(self):
+        if ((self.function.fix == 'in') and self.function.symbol):
+            return BinaryInfixOperatorToSelectedPages(self.function, self.function.symbol)
+        elif ((self.function.fix == False) and self.function.name):
+            return BinaryFunctionToSelectedPages(self.function, self.function.name)
+        else:
+            raise Wave.exceptions.WaveMissingCallStrategy            
+
+    def ternary_strategy(self):
+        if ((self.function.fix == False) and self.function.name):
+            return TernaryFunctionToSelectedPages(self.function, self.function.name)
+        else:
+            raise Wave.exceptions.WaveMissingCallStrategy 
+
+
+##
+# \todo This Menu class ought not to belong to the metamodel module.
+
+class Menu():
+
+    def __init__(self, frame, metamodel, name):
+        self.frame = frame
+        self.metamodel = metamodel
+        self.name = name
+        self.init_menus()
+        self.bind_handlers()
+
+    def create_handler(self, frame, wave_function):
+        def on(event):
+            wave_function.handler(frame)
+        return on
+
+    def init_menus(self):
+        self.frame.mm_sub_menu = wx.Menu()
+        self.frame.mm_sub_menu_items = {}
+        for wave_function in self.metamodel.functions():
+            name = self.metamodel.get_name(wave_function)
+            long_name = self.metamodel.get_long_name(wave_function)
+            self.frame.mm_sub_menu_items[name] = self.frame.mm_sub_menu.Append(wx.NewId(), name, long_name)
+        self.frame.metamodels_menu.AppendSeparator()
+        self.frame.metamodels_menu.AppendMenu(-1, self.name, self.frame.mm_sub_menu)  
+
+    def bind_handlers(self):
+        for wave_function in self.metamodel.functions():
+            name = self.metamodel.get_name(wave_function)
+            function = FunctionWithCallStrategy(wave_function)
+            handler = self.create_handler(self.frame, function)
+            self.frame.Bind(wx.EVT_MENU, handler, self.frame.mm_sub_menu_items[name])        
+
