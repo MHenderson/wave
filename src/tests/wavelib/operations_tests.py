b7bd0b9ea45f81d3543fc0f75b832921984b69b1
MHenderson <matthew.james.henderson@gmail.com>
2010-12-27 13:57:49 +0000

    Further testing of operations module.

--- src/tests/wavelib/operations_tests.py
+++ src/tests/wavelib/operations_tests.py
@@ -3,15 +3,32 @@
 import unittest
 
 from wavelib.operations import join
+from wavelib.operations import join2
+
+s = [('a', 'b')]
+t = [('b', 'c')]
 
 class TestJoin(unittest.TestCase):
-    """Test join operation."""
+
+    """
+    Test join operation.
+    """
 
     def setUp(self):
-        self.s = [('a', 'b')]
-        self.t = [('b', 'c')]
-        self.st = [('a', 'c')]
+        pass
 
     def test_join(self):
-        self.assertEqual(join(self.s,self.t), self.st)
+        self.assertEqual(join(s, t), [('a', 'c')])
+
+class TestJoin2(unittest.TestCase):
+
+    """
+    Test join2 operation.
+    """
+
+    def setUp(self):
+        pass
+
+    def test_join2(self):
+        self.assertEqual(join2(s, t), [('a', 'b', 'c')])
 
