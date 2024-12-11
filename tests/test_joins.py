092f520f218a24b3d2c953b513a104ef829547ec
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-12-11 09:41:47 +0000

    Remove nearly everything.

---
+++ tests/test_joins.py
@@ -0,0 +1,8 @@
+import Wave as wv
+
+def test_join():
+    assert wv.join([('a', 'b')], [('b', 'c')]) == [('a', 'c')]
+
+def test_join2():
+    assert wv.join([('a', 'b')], [('b', 'c')]) == [('a', 'b', 'c')]
+
